8bbe8cea6d0b1de9a177262b78e72cdd8d6d613c
Bilal Syed Hussain <bh246@st-andrews.ac.uk>
2013-06-03 21:41:57 +0100

    scripts for jenkins

---
+++ scripts/test/refine/refine_single.sh
@@ -0,0 +1,58 @@
+#/bin/bash
+
+set -o nounset
+
+echo "conjure repository is at ${CONJURE_REPO}"
+
+COUNT_ESSENCE=$(ls -1 *.essence 2> /dev/null | wc -l)
+COUNT_PARAM=$(ls -1 *.param 2> /dev/null | wc -l)
+COUNT_SOLUTION=$(ls -1 *.solution 2> /dev/null | wc -l)
+
+if (( $COUNT_ESSENCE != 1 )); then
+    WD="$(pwd)"
+    echo "ERROR: Only 1 *.essence file should be in: $WD"
+    exit 1
+fi
+
+
+if (( $# != 1 )); then
+    echo "ERROR: Give a single parameter, mode to be used by Conjure."
+    echo "       Options: {df, compact, random, first}"
+    exit 1
+fi
+
+export WD="$(pwd)"
+
+export MODE=$1
+export FAIL_FILE="${MODE}_fail.txt"
+export PASS_FILE="${MODE}_pass.txt"
+
+export SPEC=$(ls -1 *.essence | head -n 1)
+export SPEC=${SPEC%.essence}
+
+export OUT_DIR="$SPEC-$MODE"
+if [ $MODE == "df" ] ; then
+    OUT_DIR="${SPEC}-df"
+elif [ $MODE == "df-no-channelling" ] ; then
+    OUT_DIR="${SPEC}-df-no-channelling"
+fi
+
+
+rm -f "$FAIL_FILE" "$PASS_FILE"
+touch "$FAIL_FILE" "$PASS_FILE"
+
+rm -rf "$OUT_DIR"
+mkdir -p "$OUT_DIR"
+if [[ $MODE == df* ]] ; then
+    conjure --mode $MODE --in "$SPEC.essence"                               +RTS -M16G -s 2> >(tee "${MODE}_conjure.stats" >&2)
+else
+    conjure --mode $MODE --in "$SPEC.essence" --out "$OUT_DIR/$MODE.eprime" +RTS -M16G -s 2> >(tee "${MODE}_conjure.stats" >&2)
+fi
+
+NB_EPRIMES=$(ls -1 "$OUT_DIR"/*.eprime 2> /dev/null | wc -l)
+
+if (( $NB_EPRIMES == 0 )) ; then
+    echo "[generatesZeroModels] $MODE $WD" >> "$FAIL_FILE"
+else
+    echo "[refine] $MODE $WD" >> "$PASS_FILE"
+fi
