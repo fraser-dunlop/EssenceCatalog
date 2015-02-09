9cbd13b7cd42a235d94e2a81834437a96c9bf37e
Ozgur Akgun <ozgurakgun@gmail.com>
2015-02-09 14:54:17 +0000

    turning off channelling for the compact case, so we get simpler file names. also disabling prob131-noch

--- recomputation/commands.sh
+++ recomputation/commands.sh
@@ -2,13 +2,13 @@
 # timestamp: 20141203-115734
 function conjure_noch {
     parallel --tag --joblog parallel-joblog-noch --results parallel-results-noch \
-        "conjure --smart-filenames -q f -a x {} -o {.}-noch --channelling=no" ::: $(find problems -name *.essence)
+        "conjure --smart-filenames -q f -a x {} -o {.}-noch --channelling=no" ::: $(find problems -name *.essence | grep -v prob131)
     find parallel-results-noch -type f -size 0 -delete
 }
 
 # timestamp: 20141203-115937
 function conjure_compact {
     parallel --tag --joblog parallel-joblog-compact --results parallel-results-compact \
-        "conjure --smart-filenames -q f -a c {} -o {.}-compact" ::: $(find problems -name *.essence)
+        "conjure --smart-filenames -q f -a c {} -o {.}-compact --channelling=no" ::: $(find problems -name *.essence)
     find parallel-results-compact -type f -size 0 -delete
 }
