61365171226ba0e1940b9aa9e799da99af4e5731
Ozgur Akgun <ozgurakgun@gmail.com>
2015-03-25 09:59:02 +0000

    fixing givens + auxiliaries + quantifieds + cuts to Compact to reduce the number of models generated

--- recomputation/commands.sh
+++ recomputation/commands.sh
@@ -7,10 +7,11 @@ function gen_eprimes {
     # prepare the commands to be run
     find problems -name "*.essence" > essences_all.temp
     cp essences_all.temp essences_compact.temp
-    cat essences_all.temp | grep -v 'prob131\|prob116\|prob115\|prob123\|prob038\|prob031\|prob037\|prob128' > essences_noch.temp
+    # cat essences_all.temp | grep -v 'prob131\|prob116\|prob115\|prob123\|prob038\|prob031\|prob037\|prob128' > essences_noch.temp
+    cp essences_all.temp essences_noch.temp
     parallel -j1 "echo conjure --smart-filenames -qf -ac {} --channelling=no -o {.}-compact" \
         :::: essences_compact.temp > all_commands.temp
-    parallel -j1 "echo conjure --smart-filenames -qf -ax {} --channelling=no -o {.}-noch" \
+    parallel -j1 "echo conjure --smart-filenames -qf -ax {} --channelling=no -o {.}-noch --representations-givens=c --representations-auxiliaries=c --representations-quantifieds=c --representations-cuts=c" \
         :::: essences_noch.temp >> all_commands.temp
 
     # run the commands
