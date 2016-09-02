7def6786878357b6e84ed41b2503e048aaa14c51
Özgür Akgün <ozgurakgun@gmail.com>
2016-09-02 19:24:14 +0100

    allow the gen_solutions step to fail, due to SR timeouts

--- scripts/solutions/recompute.sh
+++ scripts/solutions/recompute.sh
@@ -8,7 +8,7 @@ export LIMIT_TIME=${LIMIT_TIME:-60}
 
 nb_cores=$1
 scripts/solutions/gen_conjure_commands.sh
-scripts/solutions/gen_solutions.sh $nb_cores
+scripts/solutions/gen_solutions.sh $nb_cores || true        # allowed to fail due to SR timeouts
 scripts/solutions/collect_info.sh
 scripts/solutions/clean_up.sh
 
