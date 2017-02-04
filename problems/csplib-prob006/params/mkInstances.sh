6f81273f3ff544303f981de43aaf0da3976f4378
Özgür Akgün <ozgurakgun@gmail.com>
2017-02-04 23:14:54 +0000

    Adding some params for Golomb Ruler

--- problems/csplib-prob006/params/mkInstances.sh
+++ problems/csplib-prob006/params/mkInstances.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # run to generate *.param files from instances.txt
-# gseq -f'%02g' 1 10 | bash mkInstances.sh
+# seq -w 1 10 | bash mkInstances.sh
 
 COMMAND=$( cat <<EOF
 echo "language Essence 1.3" >   {1}.param ;
