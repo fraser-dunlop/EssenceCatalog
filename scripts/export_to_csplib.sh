3e7f0687ec5e16e54afd30b1765852e702fda045
Özgür Akgün <ozgurakgun@gmail.com>
2018-05-08 10:53:36 +0100

    Update the export_to_csplib script to skip if a problem doesn't exist on CSPLib

--- scripts/export_to_csplib.sh
+++ scripts/export_to_csplib.sh
@@ -10,7 +10,9 @@ pushd problems > /dev/null                              # go through all csplib 
 for prob in csplib-*;
 do
     prob_base=${prob:7}
-    cp "${prob}/"*.essence "${ROOT_DIR}/csplib/Problems/${prob_base}/models"
+    if [ -d "${ROOT_DIR}/csplib/Problems/${prob_base}" ]; then
+        cp "${prob}/"*.essence "${ROOT_DIR}/csplib/Problems/${prob_base}/models"
+    fi
 done
 popd > /dev/null
 
